0d728c622d272a8d95df0ab9e839f5e1cecdd44a
ndolocr <crndolo@gmail.com>
2025-05-28 07:39:43 +0300

    Project Initialization

---
+++ cash-deposit/src/main/java/com/example/cash_deposit/CashDepositApplication.java
@@ -0,0 +1,13 @@
+package com.example.cash_deposit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashDepositApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CashDepositApplication.class, args);
+	}
+
+}
---
+++ cash-deposit/src/main/java/com/example/cash_deposit/customer/CustomerController.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.example.cash_deposit.customer;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author user
+ */
+@RestController
+@RequestMapping("/customer")
+public class CustomerController {
+    
+}
